fe66208098bcbcdecceb4e77fb8dfaeed55d9575
tommycrowley10 <tommycrowley10@gmail.com>
2025-01-03 13:57:24 -0600

    Added CRUD for skills

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,2 +1,32 @@
 class SkillsController < ApplicationController
+  def index
+    @skills = Skill.all
+    render :index
+  end
+
+  def create
+    @skill = Skill.create(
+      name: params[:name],
+    )
+    render :show
+  end
+
+  def show
+    @skill = Skill.find_by(id: params[:id])
+    render :show
+  end
+
+  def update
+    @skill = Skill.find_by(id: params[:id])
+    @skill.update(
+      name: params[:name] || @skill.name,
+    )
+    render :show
+  end
+
+  def destroy
+    @skill = Skill.find_by(id: params[:id])
+    @skill.destroy
+    render json: { message: "Skill destroyed successfully" }
+  end
 end
--- test/controllers/skills_controller_test.rb
+++ test/controllers/skills_controller_test.rb
@@ -1,7 +1,42 @@
 require "test_helper"
 
 class SkillsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index" do
+    get "/skills.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal Skill.count, data.length
+  end
+
+  test "create" do
+    assert_difference "Skill.count", 1 do
+      post "/skills.json"
+      assert_response 200
+    end
+
+    test "show" do
+      get "/skills/#{Skill.first.id}.json"
+      assert_response 200
+
+      data = JSON.parse(response.body)
+      assert_equal ["id", "name", "created_at", "updated_at"], data.keys
+    end
+
+    test "update" do
+      skill = Skill.first
+      patch "/skills/#{photo.id}.json", params: { name: "Updated name" }
+      assert_response 200
+
+      data = JSON.parse(response.body)
+      assert_equal "Updated name", data["name"]
+    end
+
+    test "destroy" do
+      assert_difference "Skill.count", -1 do
+        delete "/skills/#{Skill.first.id}.json"
+        assert_response 200
+      end
+    end
+  end
 end
