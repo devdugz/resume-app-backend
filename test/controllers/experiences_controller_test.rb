dccad26efb65de7f655f9c9ca982a35d11ba2d5b
realcdugz <connorduggan30@gmail.com>
2025-01-03 14:00:41 -0600

    updated controller tests

--- test/controllers/experiences_controller_test.rb
+++ test/controllers/experiences_controller_test.rb
@@ -1,7 +1,38 @@
 require "test_helper"
 
 class ExperiencesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    super
+    @experience = Experience.create!(
+      start_date: "2020",
+      end_date: "Present",
+      job_title: "Test Job",
+      company_name: "Test Company",
+      details: "Test Details",
+      user_id: @user.id,
+    )
+  end
+
+  test "show" do
+    get "/experiences/#{@experience.id}.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal ["id", "start_date", "end_date", "job_title", "company_name", "details", "created_at", "updated_at", "user_id"], data.keys
+  end
+
+  test "update" do
+    patch "/experiences/#{@experience.id}.json", params: { job_title: "Updated Title" }
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal "Updated Title", data["job_title"]
+  end
+
+  test "destroy" do
+    assert_difference "Experience.count", -1 do
+      delete "/experiences/#{@experience.id}.json"
+      assert_response 200
+    end
+  end
 end
--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -1,7 +1,37 @@
 require "test_helper"
 
 class ProjectsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    super
+    @project = Project.create!(
+      name: "Test Project",
+      description: "Test Description",
+      url: "test.com",
+      screenshot: "test.jpg",
+      user_id: @user.id,
+    )
+  end
+
+  test "show" do
+    get "/projects/#{@project.id}.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal ["id", "name", "description", "url", "screenshot", "created_at", "updated_at", "user_id"], data.keys
+  end
+
+  test "update" do
+    patch "/projects/#{@project.id}.json", params: { name: "Updated Project" }
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal "Updated Project", data["name"]
+  end
+
+  test "destroy" do
+    assert_difference "Project.count", -1 do
+      delete "/projects/#{@project.id}.json"
+      assert_response 200
+    end
+  end
 end
