84847750650123646d24d31285fbf474c8614127
realcdugz <connorduggan30@gmail.com>
2025-01-03 14:05:35 -0600

    updated skills controller & test

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,32 +1,48 @@
 class SkillsController < ApplicationController
+  before_action :authenticate_user, except: [:create, :index, :show]
+
   def index
     @skills = Skill.all
-    render :index
+    render json: @skills
   end
 
   def create
-    @skill = Skill.create(
-      name: params[:name],
+    @skill = Skill.new(
+      skill_name: params[:skill_name],
+      user_id: current_user.id,
     )
-    render :show
+    if @skill.save
+      render json: @skill
+    else
+      render json: { errors: @skill.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def show
-    @skill = Skill.find_by(id: params[:id])
-    render :show
+    @skill = Skill.find(params[:id])
+    render json: @skill
   end
 
   def update
-    @skill = Skill.find_by(id: params[:id])
-    @skill.update(
-      name: params[:name] || @skill.name,
-    )
-    render :show
+    @skill = Skill.find(params[:id])
+    if @skill.user_id == current_user.id
+      if @skill.update(skill_name: params[:skill_name])
+        render json: @skill
+      else
+        render json: { errors: @skill.errors.full_messages }, status: :unprocessable_entity
+      end
+    else
+      render json: { error: "Unauthorized" }, status: :unauthorized
+    end
   end
 
   def destroy
-    @skill = Skill.find_by(id: params[:id])
-    @skill.destroy
-    render json: { message: "Skill destroyed successfully" }
+    @skill = Skill.find(params[:id])
+    if @skill.user_id == current_user.id
+      @skill.destroy
+      render json: { message: "Skill successfully deleted" }
+    else
+      render json: { error: "Unauthorized" }, status: :unauthorized
+    end
   end
 end
